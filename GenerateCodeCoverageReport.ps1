8882bc312076f5c96c53958c03b0a76e737cf316
Nicklas Millard <n.millard@me.com>
2021-09-29 10:27:26 +0200

    use other path

--- GenerateCodeCoverageReport.ps1
+++ GenerateCodeCoverageReport.ps1
@@ -7,4 +7,4 @@ Run the reportgenerator tool to combine and generate a code coverage report
 Set-Variable -Name ReportPath -Value ./CodeCoverage/Report
 
 if(Test-Path $ReportPath) { Remove-Item $ReportPath -Recurse }
-reportgenerator -reports:./CodeCoverage/**/*cobertura.xml -targetdir:$ReportPath -reporttypes:HtmlInline_AzurePipelines
+reportgenerator -reports:./**/*cobertura.xml -targetdir:$ReportPath -reporttypes:HtmlInline_AzurePipelines
