9e0f5b6ef9009feff460eacd27f2b63a7ffa6a01
Nicklas Millard <n.millard@me.com>
2021-10-14 08:18:22 +0200

    Add book to seed data

--- src/DataLayer/Configurations/Seeds/BookTypeSeeding.cs
+++ src/DataLayer/Configurations/Seeds/BookTypeSeeding.cs
@@ -24,7 +24,8 @@ namespace DataLayer.Configurations.Seeds {
             builder.HasData(
                 new { id = Guid.Parse("26f40fdf-8f92-4c4f-80c1-71090d86aef4"), Genre = "Horror" },
                 new { id = Guid.Parse("1b0f8308-feb0-4d55-93ec-0765971e0bb7"), Genre = "Fiction" },
-                new { id = Guid.Parse("5f7f47e3-610f-499c-9119-b73e1df23b62"), Genre = "Crime" }
+                new { id = Guid.Parse("5f7f47e3-610f-499c-9119-b73e1df23b62"), Genre = "Crime" },
+                new { id = Guid.Parse("804B81BF-1730-43C8-AEEB-DE6685E41CC3"), Genre = "Other" }
             );
         }
     }
